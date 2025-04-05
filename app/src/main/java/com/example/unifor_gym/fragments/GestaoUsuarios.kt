0c835ade16cb22b14c87733be98175e1a5a2269d
matheusclima <matheuslima@pwrgestao.com>
2025-04-05 20:33:22 -0300

    refactor: Reorganização do projeto em activities e fragments; feat: Criação de um menu de navegação para a activity do Admin.

---
+++ app/src/main/java/com/example/unifor_gym/fragments/GestaoUsuarios.kt
@@ -0,0 +1,23 @@
+package com.example.unifor_gym.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.unifor_gym.R
+
+class GestaoUsuarios : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gestao_usuarios, container, false)
+    }
+}
