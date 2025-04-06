1b667565b88ead77dcda5fa2939874369eb27586
matheusclima <matheuslima1607@gmail.com>
2025-04-06 00:49:20 -0300

    feat: Criação da página de gestão de exercícios utilizando mock data em um Recycler View.

---
+++ app/src/main/java/com/example/unifor_gym/adapters/ExercicioAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.unifor_gym.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.unifor_gym.R
+import com.example.unifor_gym.fragments.Exercicio
+
+class ExercicioAdapter (
+    private val lista: List<Exercicio>,
+    private val onMoreClick: (Exercicio) -> Unit
+) : RecyclerView.Adapter<ExercicioAdapter.ExercicioViewHolder>() {
+
+    inner class ExercicioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtNome: TextView = itemView.findViewById(R.id.txtNome)
+        val txtNivel: TextView = itemView.findViewById(R.id.txtNivel)
+        val txtGrupoMuscular: TextView = itemView.findViewById(R.id.txtGrupoMuscular)
+        val btnMore: ImageView = itemView.findViewById(R.id.btnMoreItemExercicio)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExercicioViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_exercicio, parent, false)
+        return ExercicioViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ExercicioViewHolder, position: Int) {
+        val exercicio = lista[position]
+        holder.txtNome.text = exercicio.nome
+        holder.txtNivel.text = exercicio.nivel
+        holder.txtGrupoMuscular.text = exercicio.grupoMuscular
+
+        holder.btnMore.setOnClickListener {
+            onMoreClick(exercicio)
+        }
+    }
+
+    override fun getItemCount(): Int = lista.size
+}
--- app/src/main/java/com/example/unifor_gym/fragments/GestaoExercicios.kt
+++ app/src/main/java/com/example/unifor_gym/fragments/GestaoExercicios.kt
@@ -1,17 +1,28 @@
 package com.example.unifor_gym.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.unifor_gym.R
+import com.example.unifor_gym.adapters.ExercicioAdapter
+
+data class Exercicio (
+    val nome: String,
+    val nivel: String,
+    val grupoMuscular: String
+)
 
 class GestaoExercicios : Fragment() {
+    private lateinit var recyclerExercicios: RecyclerView
+    private lateinit var listaDeExercicios: List<Exercicio>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -20,5 +31,25 @@ class GestaoExercicios : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gestao_exercicios, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerExercicios = view.findViewById(R.id.recyclerExercicios)
+
+        listaDeExercicios = listOf(
+            Exercicio("Supino", "Intermediário", "Peito"),
+            Exercicio("Agachamento", "Intermediário", "Pernas"),
+            Exercicio("Levantamento Terra", "Avançado", "Costas"),
+            Exercicio("Rosca direta", "Iniciante", "Braços")
+        )
+
+        val exerciciosAdapter = ExercicioAdapter(listaDeExercicios) { exercicio ->
+            Log.d("Menu de Administração do Exercício", "onMoreClick: " + exercicio.nome)
+        }
+
+        recyclerExercicios.layoutManager = LinearLayoutManager(requireContext())
+        recyclerExercicios.adapter = exerciciosAdapter
+
     }
 }
