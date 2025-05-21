944d8aa253b7215917b89331ab6d3afc25a4525a
matheusclima <matheuslima1607@gmail.com>
2025-05-21 10:09:29 -0300

    feat: add firebase integration and get classes from firebase

--- app/src/main/java/com/example/unifor_gym/activities/AulaDetalhes.kt
+++ app/src/main/java/com/example/unifor_gym/activities/AulaDetalhes.kt
@@ -1,7 +1,10 @@
 package com.example.unifor_gym.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -11,14 +14,31 @@ import com.example.unifor_gym.R
 
 class AulaDetalhes : AppCompatActivity() {
     lateinit var btnVoltar: CardView
-
+    lateinit var txtNome: TextView
+    lateinit var txtHorarioInicio: TextView
+    lateinit var txtHorarioFim: TextView
+    lateinit var txtMatriculados: TextView
+    lateinit var txtVagas: TextView
+    lateinit var txtEquipamentos: TextView
+    lateinit var txtDiaSemana: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aula_detalhes)
         btnVoltar = findViewById(R.id.btnVoltarAulaDetalhes)
-
         btnVoltar.setOnClickListener {
             finish()
         }
+
+        val aulaId = intent.getStringExtra("aulaId")
+        Log.d("AulaDetalhes", "Aula:" + aulaId)
+        if(aulaId.isNullOrEmpty()) {
+            Toast.makeText(this, "Aula não encontrada", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        } else {
+            Toast.makeText(this, "Aula encontrada: " + aulaId, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
